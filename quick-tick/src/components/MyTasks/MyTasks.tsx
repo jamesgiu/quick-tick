95a52b9fb47acb048d6d7c7020a9f605bb3f6d3d
jamesgiu <james.giuffrida@gmail.com>
2023-01-04 01:45:44 +1100

    QT-20 add xxs

--- quick-tick/src/components/MyTasks/MyTasks.tsx
+++ quick-tick/src/components/MyTasks/MyTasks.tsx
@@ -69,11 +69,11 @@ export default function MyTasks(): JSX.Element {
         <NewTask/>
         <ResponsiveGridLayout
             className="layout"
-            layouts={{'lg': layout, 'md': layout, 'sm': layout, 'xs': layout}}
+            layouts={{'lg': layout, 'md': layout, 'sm': layout, 'xs': layout, 'xxs': layout}}
             onDragStop={(layout) => setLayout(layout)}
             onResizeStop={(layout) => setLayout(layout)}
             rowHeight={5}
-            cols={{'lg': 12, 'md': 12, 'sm': 12, 'xs': 12}}
+            cols={{'lg': 12, 'md': 12, 'sm': 12, 'xs': 12, 'xxs': 12}}
         >
             {getTaskListPanels()}
        </ResponsiveGridLayout>
