f27eb81532d4374df596344658716d9e26ece445
jamesgiu <james.giuffrida@gmail.com>
2023-01-13 01:01:34 +1100

    QT-20 Only set live task stats when taskListMap changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -42,7 +42,6 @@ export default function Home(): JSX.Element {
 
     useEffect(()=> {
         setInspirationalQuote(Quote.getQuote());
-        setLiveTaskStats();
     }, []);
 
     useEffect(()=> {
